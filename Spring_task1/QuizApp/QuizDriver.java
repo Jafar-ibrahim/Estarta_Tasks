caae94bbee9fcdc2aec0c61af753aac80e0cbacc
Jafar Farwaneh <89139536+Jafar-ibrahim@users.noreply.github.com>
2023-08-22 12:58:32 +0300

    Add files via upload

--- Spring_task1/QuizApp/QuizDriver.java
+++ Spring_task1/QuizApp/QuizDriver.java
@@ -5,6 +5,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class QuizDriver {
     public static void main(String[] args) {
 
+        // xml file is found in the resources directory
         BeanFactory ctx = new
                 ClassPathXmlApplicationContext("springConfig.xml");
         Quiz quiz = (Quiz)ctx.getBean("quiz");
