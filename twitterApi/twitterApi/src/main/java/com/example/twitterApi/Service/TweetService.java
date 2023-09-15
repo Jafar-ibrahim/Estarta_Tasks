96782d5853c74e8031fb3f92266732e35d6b1569
Jafar Farwaneh <89139536+Jafar-ibrahim@users.noreply.github.com>
2023-09-15 03:56:25 +0300

    Delete twitterApi directory

--- twitterApi/twitterApi/src/main/java/com/example/twitterApi/Service/TweetService.java
+++
@@ -1,9 +0,0 @@
-package com.example.twitterApi.Service;
-
-import com.example.twitterApi.Entity.TweetDto;
-
-import java.util.List;
-
-public interface TweetService {
-    TweetDto postTweet(String userName, String body);
-}
